1766cfa2fd4607b869ee2f1d062f60af09c32940
JoeToscani <39446512+Episode353@users.noreply.github.com>
2023-12-11 08:56:37 -0500

    added empty pages

--- jwodemo/myapp/urls.py
+++ jwodemo/myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("seepcoin", views.seepcoin, name="seepcoin"),
+    path("board", views.board, name="board"),
+    path("blog", views.blog, name="blog"),
+    path("foodreview", views.foodreview, name="foodreview")
 ]
--- jwodemo/myapp/views.py
+++ jwodemo/myapp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def seepcoin(request):
+    return render(request, "seepcoin.html")
+    
+def board(request):
+    return render(request, "board.html")
+
+def blog(request):
+    return render(request, "blog.html")
+    
+def foodreview(request):
+    return render(request, "foodreview.html")
