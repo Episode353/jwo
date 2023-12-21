8bd32d0eaad304fee950ba9faa9bb293f48e0419
JoeToscani <joe.v.toscano@gmail.com>
2023-12-20 23:49:16 -0500

    updated registration form

---
+++ jwodemo/members/forms.py
@@ -0,0 +1,13 @@
+# forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs['class'] = 'form-control'
