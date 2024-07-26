c7b95d28c0eb6ad48e4dd34febecc8f859dff7ea
JoeToscani <39446512+Episode353@users.noreply.github.com>
2024-07-26 15:30:37 -0400

    updated home page, added login message
    updated the message on the home page
    if a user goes to a login required page, but isnt logged in, it will redirect them to the login page and have a "you must be logged in" message

--- jwodemo/members/views.py
+++ jwodemo/members/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from functools import wraps
 
 def login_user(request):
     if request.method == "POST":
@@ -19,14 +18,12 @@ def login_user(request):
             # Return an 'invalid login' error message.
             messages.success(request, "There was an error logging in, try again....")
             return redirect('login')
-
-
     else:
         return render(request, 'authenticate/login.html', {})
-    
+
 def logout_user(request):
     logout(request)
-    messages.success(request, "You have been Logged out.")
+    messages.success(request, "You have been logged out.")
     return redirect('home')
 
 def register_user(request):
@@ -46,18 +43,25 @@ def register_user(request):
         'form': form,
     })
 
-@login_required
+def login_required_with_message(view_func):
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            messages.success(request, "You must be logged in to view this page.")
+            return redirect('login')
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+@login_required_with_message
 def account(request):
     profile = request.user.profile
     return render(request, "account.html", {'profile': profile})
 
-@login_required
+@login_required_with_message
 def edit_profile(request):
     if request.method == "POST":
         new_name = request.POST.get('name')
         request.user.profile.name = new_name
         request.user.profile.save()
         return redirect('account')
-
     return render(request, 'edit_profile.html')
-
